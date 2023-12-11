201399311a01ada421ec6733ac65b1f434484bb4
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-12-11 19:15:41 +0200

    add test project files

---
+++ src/redux/operations.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const carsApi = createApi({
+  reducerPath: 'carsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://6551ddf85c69a77903292ee4.mockapi.io/',
+  }),
+  tagTypes: ['Advert'],
+  endpoints: builder => ({
+    getCarsByPage: builder.query({
+      query: (page = 1) => `advert?page=${page}&limit=8`,
+    }),
+    getAdverts: builder.query({
+      query: () => '/advert',
+      providesTags: ['Advert'],
+    }),
+  }),
+});
+
+export const { useGetCarsByPageQuery, useGetAdvertsQuery } = carsApi;
